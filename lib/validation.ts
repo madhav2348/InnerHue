9b0d704ec13ad38214ae7ab9caa8c0b6a87bb61b
Madhav Majumdar <161720210+madhav2348@users.noreply.github.com>
2026-02-13 15:29:25 +0530

    testing

--- lib/validation.ts
+++ lib/validation.ts
@@ -10,7 +10,7 @@ export const LoginSchema = z.object({
 // 2. Signup Schema
 export const SignupSchema = z.object({
   name: z.string().min(2, { message: "Name must be at least 2 characters." }),
-  email: z.string().email({ message: "Please enter a valid email address." }),
+  email: z.string().email({ message: "Please enter a valid email ." }),
   password: z.string().min(8, { message: "Password must be at least 8 characters." })
     .regex(/[A-Z]/, { message: "Password must contain at least one uppercase letter." })
     .regex(/[0-9]/, { message: "Password must contain at least one number." }),
